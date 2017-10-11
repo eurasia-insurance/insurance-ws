ba4c621f7b37ca786b8607a8e004ae590582fe4e
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-10-11 12:56:20 +0600

    using new BeanUtils method lookup

--- src/main/java/tech/lapsa/insurance/ws/jaxb/validator/constraint/PolicyDriverSettingsValidConstraintValidator.java
+++ src/main/java/tech/lapsa/insurance/ws/jaxb/validator/constraint/PolicyDriverSettingsValidConstraintValidator.java
@@ -1,11 +1,11 @@
 package tech.lapsa.insurance.ws.jaxb.validator.constraint;
 
-import static com.lapsa.utils.BeanUtils.*;
-
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import javax.validation.ValidationException;
 
 import com.lapsa.insurance.domain.policy.PolicyDriver;
+import com.lapsa.utils.BeanUtils;
 
 import tech.lapsa.insurance.facade.PolicyDriverFacade;
 import tech.lapsa.insurance.ws.jaxb.entity.XmlPolicyDriverInfo;
@@ -24,8 +24,9 @@ public class PolicyDriverSettingsValidConstraintValidator
 	if (value == null)
 	    return true;
 
-	PolicyDriverFacade facade = getBean(PolicyDriverFacade.class);
-	PolicyDriver fetched = facade.fetchByIdNumber(value.getIdNumber());
+	PolicyDriver fetched = BeanUtils.lookup(PolicyDriverFacade.class) //
+		.orElseThrow(() -> new ValidationException("Cannot find an instance of " + PolicyDriverFacade.class)) //
+		.fetchByIdNumber(value.getIdNumber());
 
 	if (value.getInsuranceClass() != null && value.getInsuranceClass() != fetched.getInsuranceClassType())
 	    return invalid(context, ValidationMessages.POLICY_DRIVER_SETTINGS_VALID_INVALID_CLASS);
