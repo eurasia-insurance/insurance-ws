2deef2061b7e5c157e1b8f9280b1732698b91ce1
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-25 17:10:05 +0600

    policy calculations

--- webapp/src/main/java/tech/lapsa/insurance/ws/jaxb/validator/constraint/PolicyParametersValidConstraintValidator.java
+++ webapp/src/main/java/tech/lapsa/insurance/ws/jaxb/validator/constraint/PolicyParametersValidConstraintValidator.java
@@ -4,13 +4,15 @@ import static tech.lapsa.insurance.ws.rs.app.ConverterUtil.*;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import javax.validation.ValidationException;
 
 import com.lapsa.insurance.domain.policy.Policy;
 
 import tech.lapsa.insurance.calculation.CalculationFailed;
-import tech.lapsa.insurance.calculation.PolicyCalculation;
+import tech.lapsa.insurance.calculation.PolicyCalculation.PolicyCalculationRemote;
 import tech.lapsa.insurance.ws.jaxb.entity.XmlFetchPolicy;
 import tech.lapsa.insurance.ws.jaxb.validator.PolicyParametersValid;
+import tech.lapsa.java.commons.naming.MyNaming;
 import tech.lapsa.javax.rs.utility.WrongArgumentException;
 
 public class PolicyParametersValidConstraintValidator
@@ -25,15 +27,22 @@ public class PolicyParametersValidConstraintValidator
 	if (value == null)
 	    return true;
 
-	Policy policy;
+	final Policy policy;
 	try {
 	    policy = convertPolicyShort(value);
 	} catch (final WrongArgumentException e) {
 	    exception(context, e);
 	    return false;
 	}
+
+	final PolicyCalculationRemote policyCalculations = MyNaming.lookupEJB(ValidationException::new,
+		PolicyCalculationRemote.APPLICATION_NAME,
+		PolicyCalculationRemote.MODULE_NAME,
+		PolicyCalculationRemote.BEAN_NAME,
+		PolicyCalculationRemote.class);
+
 	try {
-	    PolicyCalculation.calculatePolicyCost(policy);
+	    policyCalculations.calculatePolicyCost(policy);
 	} catch (final CalculationFailed e) {
 	    exception(context, e);
 	    return false;
