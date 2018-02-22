80ef2f56607d4ab1854cec32ea8f935cc2445515
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-02-22 15:33:56 +0600

    normalizing number

--- webapp/src/main/java/tech/lapsa/insurance/ws/rs/app/PolicyWS.java
+++ webapp/src/main/java/tech/lapsa/insurance/ws/rs/app/PolicyWS.java
@@ -35,6 +35,7 @@ import tech.lapsa.java.commons.logging.MyLogger;
 import tech.lapsa.javax.rs.utility.InternalServerErrorException;
 import tech.lapsa.javax.rs.utility.WrongArgumentException;
 import tech.lapsa.javax.validation.NotNullValue;
+import tech.lapsa.kz.vehicle.VehicleRegNumber;
 
 @Path("/policy")
 @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
@@ -125,7 +126,9 @@ public class PolicyWS extends ALanguageDetectorWS {
     private XmlPolicyVehicleInfo _fetchVehicle(final XmlFetchPolicyVehicle request)
 	    throws WrongArgumentException, InternalServerErrorException {
 	try {
-	    final PolicyVehicle vehicle = policyVehicles.fetchFirstByRegNumberOrDefault(request.getRegNumber());
+	    final VehicleRegNumber vrn = VehicleRegNumber
+		    .assertValid(VehicleRegNumber.normalizeNumber(request.getRegNumber().getNumber()));
+	    final PolicyVehicle vehicle = policyVehicles.fetchFirstByRegNumberOrDefault(vrn);
 	    final XmlPolicyVehicleInfo response = ConverterUtil.convertXmlPolicyVehicle(vehicle);
 	    return response;
 	} catch (final IllegalArgument e) {
